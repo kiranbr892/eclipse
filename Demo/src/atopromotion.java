561319b72dc3f566d4aba36b92ee868127ee1794
EMINDSLOGIC <EMINDSLOGIC@DESKTOP-86SRNH6>
2019-11-15 08:16:39 +0530

    eclipse

---
+++ Demo/src/atopromotion.java
@@ -0,0 +1,16 @@
+
+public class atopromotion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte a=50;
+		byte b=65;
+		byte c=10;
+		byte d;	//addition of two byte is stored in int type
+				//so d is declared as int type or explicit type castiong done
+		d= (byte)(b+c);
+		System.out.println("value of d is"+d);
+
+	}
+
+}
